00701d3cbb2bf596072ec2f55ad0c4dfca817ae8
michuNEEO <michael@neeo.com>
2016-03-10 21:33:58 +0100

    squeeze out more bytes (1009b)

--- js1k.js
+++ js1k.js
@@ -35,8 +35,8 @@ setInterval(function() {
   i=32*Math.cos(u/188);
 
 	// __ draw GRID
-  c.strokeStyle = "rgba(255,0,255, 0.5)";
-  c.fillStyle = "rgba(0,0,96, 0.1)";
+  c.strokeStyle = "rgba(255,0,255,.5)";
+  c.fillStyle = "rgba(0,0,96,.1)";
   //horizontal lines
   for (l = 0; l <20; l++) {
     c.lineWidth = 2;
@@ -69,7 +69,7 @@ setInterval(function() {
   // __ draw PLANETS
   i=8*Math.cos(u/222);
 
-  c.fillStyle = "rgba(255,255,255, 0.05)";
+  c.fillStyle = "rgba(255,255,255,.05)";
   c.beginPath();
   c.arc(100, -h/8, h/2-i, 0, 2*Math.PI);
   c.arc(w, h/4, h/5+i, 0, 2*Math.PI);
@@ -77,7 +77,7 @@ setInterval(function() {
 
 	//__ draw stars
 	for (l = 0; l<1200; l++) {
-    c.fillStyle = "rgba(255,255,255, 0.25)";
+    c.fillStyle = "rgba(255,255,255,.25)";
 		if (!s[l]) {
       //init new star, array value: xpos, ypos, xsize, ysize, speed
 			s[l] = [Math.random() * w, Math.random() * h , 1 + Math.random() * 2, 1 + Math.random() * 2, Math.random() * 0.4];
@@ -88,10 +88,10 @@ setInterval(function() {
 	}
 
 	// __ draw CANONS, s: startpos, t: targetpos, l:lifetime
-  c.strokeStyle = "rgba(255,0,255, 0.3)";
+  c.strokeStyle = "rgba(255,0,255,.3)";
   c.lineWidth = 6;
 	for (l = 0; l<1200; l++) {
-    if (l==3) c.strokeStyle = "rgba(255,0,255, 0.006)";
+    if (l==3) c.strokeStyle = "rgba(255,0,255,.006)";
 		if (f[l] && f[l].l > 0) {
       c.beginPath();
 	    c.moveTo(f[l].s, h/2);
@@ -128,7 +128,7 @@ setInterval(function() {
   i=20*Math.cos(u/128);
   c.strokeStyle = "#000";
   c.lineWidth = 2;
-  c.fillStyle = "rgba(255,0,255, 0.3)";
+  c.fillStyle = "rgba(255,0,255,.3)";
   c.beginPath();
   c.lineTo(w/2, 2*h/3-i);
   c.lineTo(w/2-i+0.87*-h/3, i+ h/6 ); //top left, Math.cos(Math.PI/6) = 0.8660254037844387
